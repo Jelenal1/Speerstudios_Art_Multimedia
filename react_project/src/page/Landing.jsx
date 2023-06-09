965ed0908275363171eafe259b3ba376a7b4199c
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-09 23:15:14 +0200

    I made a page on which the background changes onscroll

--- react_project/src/page/Landing.jsx
+++ react_project/src/page/Landing.jsx
@@ -1,22 +1,65 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import bglanding from "../img/Landingbg.jpg";
+import bglanding2 from "../img/Landingbg2.jpg";
+import bglanding3 from "../img/Landingbg3.jpg";
+import { useEffect } from "react";
 
 function Landing() {
+  const style = {
+    paragraphsaying: `bg-gray-300 h-14 flex justify-center items-center font-main text-2xl`,
+    pwrapper: `h-96 my-20 flex flex-col justify-center border border-red-500`,
+  };
+  const backgrounds = [bglanding, bglanding2, bglanding3];
+  const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const sectionHeight = document.documentElement.scrollHeight;
+
+      const maxIndex = backgrounds.length - 1;
+      const index = Math.floor(
+        (scrollPosition / sectionHeight) * backgrounds.length
+      );
+      const newIndex = Math.min(index, maxIndex);
+
+      setCurrentBackgroundIndex(newIndex);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [backgrounds.length]);
+
+  const currentBackground = backgrounds[currentBackgroundIndex];
+
   return (
-    <>
+    <div
+      className="bg-cover bg-center bg-fixed min-h-screen object-cover"
+      style={{
+        backgroundImage: `url(${currentBackground})`,
+      }}
+    >
       <Navbar />
-      <div
-        className="bg-cover bg-center h-screen object-cover"
-        style={{ backgroundImage: `url(${bglanding})` }}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
-          impedit modi a id obcaecati corporis voluptatem, magnam cupiditate
-          dignissimos ipsam exercitationem non omnis magni eligendi iure unde
-          cum et quibusdam.
-        </p>
+      <div className="flex flex-col justify-around min-h-screen">
+        <div className={style.pwrapper}>
+          <p className={style.paragraphsaying}>
+            We don't make Art, we live Art
+          </p>
+        </div>
+        <div className={style.pwrapper}>
+          <p className={style.paragraphsaying}>Pure chaos and no structure</p>
+        </div>
+        <div className={style.pwrapper}>
+          <p className={style.paragraphsaying}>
+            If you want to be part of it, try it out.
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 export default Landing;

d753650cfe7468a0262618969ba3c04d70c34d40
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-09 14:36:10 +0200

    I customized my style

--- react_project/tailwind.config.js
+++ react_project/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main': ['IM Fell DW Pica', 'serif']
+      }
+    },
   },
   plugins: [],
 }

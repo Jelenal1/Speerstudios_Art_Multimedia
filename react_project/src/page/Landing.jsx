589e25d46e6f977a02f828214ddace59d30e11b9
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-10 19:15:13 +0200

    I added some content

--- react_project/src/page/Landing.jsx
+++ react_project/src/page/Landing.jsx
@@ -2,15 +2,17 @@ import Navbar from "../components/Navbar";
 import bglanding from "../img/Landingbg.jpg";
 import bglanding2 from "../img/Landingbg2.jpg";
 import bglanding3 from "../img/Landingbg3.jpg";
+import bglanding4 from "../img/Landingbg4.jpg";
 
 function Landing() {
   const style = {
     paragraphsaying: `bg-gray-300 h-14 flex justify-center items-center font-main text-2xl`,
     bgpicture: `bg-cover bg-center bg-fixed object-cover min-h-screen flex flex-col justify-center`,
+    button: "ml-1.5 text-3xl h-fit rounded-lg w-10 bg-[#F18CED]",
   };
 
   return (
-    <div>
+    <>
       <Navbar />
       <div
         className={style.bgpicture}
@@ -44,7 +46,30 @@ function Landing() {
           If you want to be part of it, try it out.
         </p>
       </div>
-    </div>
+      <div
+        className={style.bgpicture}
+        style={{
+          backgroundImage: `url(${bglanding4})`,
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+        }}
+      >
+        <p className={style.paragraphsaying}>Express your self</p>
+      </div>
+      <div
+        className={style.bgpicture}
+        style={{
+          backgroundImage: `url(${bglanding4})`,
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+        }}
+      >
+        <p className={style.paragraphsaying}>
+          Get a peace of
+          <span className="text-3xl ml-2">Us</span>
+        </p>
+      </div>
+    </>
   );
 }
 

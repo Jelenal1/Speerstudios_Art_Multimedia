7b6dd5b11d86762527b65b053406ca8e1e7f17cb
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-20 14:51:09 +0200

    feat: I added routinge to the navbar

--- react_project/src/components/Navbar.jsx
+++ react_project/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../img/Logo.png";
 
 function Navbar() {
@@ -5,13 +6,14 @@ function Navbar() {
     button: `ml-20 text-xl bg-[#F18CED] h-fit rounded-lg w-24`,
   };
 
+  const navigate = useNavigate();
   return (
     <div className="bg-gray-300 flex items-center font-main h-14 fixed w-full">
       <div className="flex items-center ml-1">
         <img src={logo} className="h-10" />
         <h1 className="text-3xl">SpeerStudios</h1>
       </div>
-      <button className={style.button}>Home</button>
+      <button className={style.button} onClick={navigate("/")}>Home</button>
       <button className={style.button}>Offerings</button>
       <button className={style.button}>About Us</button>
       <button className={style.button}>Contact</button>

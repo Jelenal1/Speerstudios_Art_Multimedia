c045fef07f565dfeb061a05c1befd148737837b3
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-23 13:52:58 +0200

    Feat: Offering page

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,11 +1,13 @@
 import Landing from "./page/Landing";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Offerings from "./page/Offerings";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/offerings" element={<Offerings />} />
       </Routes>
     </Router>
   );

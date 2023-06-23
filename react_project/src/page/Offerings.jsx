dbbb55df847d55d561880ce9674db330835aff38
jelenal1 <jabbruh10@gmail.com>
2023-06-23 18:31:15 +0200

    feat: I get data from firestore

--- react_project/src/page/Offerings.jsx
+++ react_project/src/page/Offerings.jsx
@@ -1,12 +1,25 @@
-import Navbar from '../components/Navbar.jsx';
+import Navbar from "../components/Navbar.jsx";
+import { db } from "../firebase";
+import { getCollection } from "firebase/firestore";
 
 export default function Offerings() {
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-col min-h-screen items-center">
-                <h1 className='text-3xl'>Offerings</h1>
-            </div>
-        </>
-    );
+  const [offeringsdata, setOfferingsData] = useState([]);
+
+  const getOfferings = async () => {
+    const data = await getCollection(db, "offerings");
+    setOfferingsData(data);
+  };
+
+  useEffect(() => {
+    getOfferings();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col min-h-screen items-center">
+        <h1 className="text-3xl">Offerings</h1>
+      </div>
+    </>
+  );
 }

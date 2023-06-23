d51721eb6f23a61d599427e568a67b5be15fc827
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-23 14:25:38 +0200

    style: added icons

--- react_project/src/components/Navbar.jsx
+++ react_project/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../img/Logo.png";
 import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 function Navbar() {
   const style = {
     buttonLg: `mx-10 text-xl bg-[#F18CED] h-fit rounded-lg w-24 hidden lg:block`,
     buttonSm: `text-xl bg-[#F18CED] h-fit rounded-lg w-24 block lg:hidden my-10`,
-    wrapperSm: `flex flex-col items-center bg-gray-300 fixed top-0 right-0 w-40 h-full`
+    wrapperSm: `flex flex-col items-center bg-gray-300 fixed top-0 right-0 w-32 h-full`
   };
 
   const [open, setOpen] = useState(false);
@@ -32,9 +33,9 @@ function Navbar() {
         <button className={style.buttonLg}>About Us</button>
         <button className={style.buttonLg}>Contact</button>
       </div>
-      <button className="ml-auto" onClick={handleOpen} >open</button>
+      <button className="ml-auto mr-3" onClick={handleOpen} ><AiOutlineMenu className="w-8 h-8" /></button>
       <div className={open ? style.wrapperSm : "hidden"}>
-        <button className="text-3xl mt-10" onClick={() => handleClose()}>✖️</button>
+        <button className="text-3xl mt-10" onClick={() => handleClose()}><AiOutlineClose /></button>
         <button className={style.buttonSm} onClick={() => navigate("/")}>Home</button>
         <button className={style.buttonSm} onClick={() => navigate("/offerings")}>Offerings</button>
         <button className={style.buttonSm}>About Us</button>

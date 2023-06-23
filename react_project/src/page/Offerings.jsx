73194d91be6e5dc3a533599b84679813ca2d8394
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-23 13:52:35 +0200

    first heading

---
+++ react_project/src/page/Offerings.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar.jsx';
+
+export default function Offerings() {
+    return (
+        <>
+            <Navbar />
+            <div className="flex flex-col min-h-screen items-center">
+                <h1 className='text-3xl'>Offerings</h1>
+            </div>
+        </>
+    );
+}

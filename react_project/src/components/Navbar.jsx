ddffab38add9aa37d8b1e70f2fd74960eec62b77
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-23 11:44:26 +0200

    style: sticky seems rounder

--- react_project/src/components/Navbar.jsx
+++ react_project/src/components/Navbar.jsx
@@ -8,13 +8,13 @@ function Navbar() {
 
   const navigate = useNavigate();
   return (
-    <div className="bg-gray-300 flex items-center font-main h-14 fixed w-full">
+    <div className="bg-gray-300 flex items-center font-main h-14 sticky w-full">
       <div className="flex items-center ml-1">
         <img src={logo} className="h-10" />
         <h1 className="text-3xl">SpeerStudios</h1>
       </div>
       <button className={style.button} onClick={() => navigate("/")}>Home</button>
-      <button className={style.button}>Offerings</button>
+      <button className={style.button} onClick={() => navigate("/offerings")}>Offerings</button>
       <button className={style.button}>About Us</button>
       <button className={style.button}>Contact</button>
     </div>

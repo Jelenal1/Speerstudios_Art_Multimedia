ec6de0e250d1bed6331b9af0319355102d2a8041
jelenal1 <jabbruh10@gmail.com>
2023-06-27 18:05:52 +0200

    feat: added page not found error

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,6 +1,7 @@
 import Landing from "./page/Landing";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Offerings from "./page/Offerings";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -8,6 +9,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/offerings" element={<Offerings />} />
+        <Route
+          path="/*"
+          element={
+            <>
+              <Navbar />
+              <h1 className="text-3xl text-center mt-5">404 Page Not Found</h1>
+            </>
+          }
+        />
       </Routes>
     </Router>
   );

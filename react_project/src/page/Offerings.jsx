31cfd12b243b42a7ed629b98f4c27c7fecd1c3c1
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-27 11:23:34 +0200

    style: first styled version of the carousel

--- react_project/src/page/Offerings.jsx
+++ react_project/src/page/Offerings.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar.jsx";
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import Image from "../components/Image.jsx";
+import { Carousel } from "@material-tailwind/react";
 
 export default function Offerings() {
   const [offeringsdata, setOfferingsData] = useState([]);
@@ -25,22 +26,30 @@ export default function Offerings() {
     getOfferings();
   }, []);
 
+  const style = {
+    button: "text-xl bg-[#F18CED] h-fit rounded-lg w-24",
+  };
+
   return (
     <>
       <Navbar />
-      <div className="flex flex-col min-h-screen items-center">
+      <div className="flex flex-col min-h-screen items-center font-main">
         <h1 className="text-3xl">Offerings</h1>
         {offeringsdata.map((item) => {
           return (
-            <div key={item.id} className="flex flex-col items-center mt-10">
+            <div key={item.id} className="flex flex-col items-center mt-5">
               <h2 key={item.id} className="text-2xl">{item.title}</h2>
-              {item.imagenames.map((image) => {
-                return (
-                  <div className="flex justify-center aspect-square w-1/3 border-2" key={image}>
-                    <Image imagename={image} />
-                  </div>
-                )
-              })}
+              <Carousel className=" border-2 w-[800px] h-[605px]">
+                {item.imagenames.map((image) => {
+                  return (
+                    <div key={image} className="flex justify-center w-[800px] h-[600px]">
+                      <Image imagename={image} />
+                    </div>
+                  )
+                })}
+              </Carousel>
+              <h2 className="text-2xl">{item.price + " CHF"}</h2>
+              <button className={style.button}>Buy</button>
             </div>
           );
         })}

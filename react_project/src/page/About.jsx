8c8dad01951e6122ac584096788f6d511e178270
jelenal1 <jabbruh10@gmail.com>
2023-06-27 22:35:36 +0200

    added more content

--- react_project/src/page/About.jsx
+++ react_project/src/page/About.jsx
@@ -1,28 +1,42 @@
 import Navbar from "../components/Navbar.jsx";
 
 export default function About() {
- const style = {
+  const style = {
     maincontainer: "flex flex-col min-h-screen items-center font-main",
     button: "text-xl bg-[#F18CED] h-fit rounded-lg w-24",
-    paragraphsaying: `bg-gray-300 p-2 my-10 w-full flex flex-col items-center text-center font-main text-2xl`
+    paragraphsaying: `bg-gray-300 p-2 my-10 w-full flex flex-col items-center text-center font-main text-2xl`,
   };
 
   return (
     <>
-    <Navbar />
-    <div className={style.maincontainer}>
-      <h1 className="text-3xl mt-2">About</h1>
-      <div className={style.paragraphsaying}>
-      <p>Hi there, glad to see you.
-    <br />This is the about page.
-      <br/> <strong> A quick disclaimer </strong> <br /> you may want to read,<br/>
-      who we are.
-      <br /> But we are nothing
-      <br /> And No One.
-      </p>
+      <Navbar />
+      <div className={style.maincontainer}>
+        <h1 className="text-3xl mt-2">About</h1>
+        <div className={style.paragraphsaying}>
+          <p>
+            Hi there, glad to see you.
+            <br />
+            This is the about page.
+            <br /> <strong> A quick disclaimer </strong> <br /> you may want to
+            read,
+            <br />
+            who we are.
+            <br /> But we are nothing
+            <br /> And No One.
+          </p>
+        </div>
+        <div className={style.paragraphsaying}>
+          <p>
+            We are a variety of artists. We are not interested in being famous
+            or publicly known. <br />
+            We just want to make our art and support others to make art too.{" "}
+            <br />
+            We are not looking for profit, we just want enough money for food
+            and more art. <br />
+            This is not just a cheesy catch phrase. We live art. <br />
+          </p>
+        </div>
       </div>
-      
-    </div>
     </>
   );
 }

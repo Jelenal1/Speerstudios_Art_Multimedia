4259a9f86bb1504adcf28ca3549c1c11ec4773b5
jelenal1 <jabbruh10@gmail.com>
2023-06-27 18:26:53 +0200

    feat: the loggin is now functioning

--- react_project/src/page/Login.jsx
+++ react_project/src/page/Login.jsx
@@ -1,20 +1,39 @@
 import Navbar from "../components/Navbar";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function Login() {
+  const loggin = async (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen items-center font-main">
+    <>
       <Navbar />
-      <h1 className="text-3xl">Login</h1>
-      <form className="flex flex-col mt-2">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" className="border-2 rounded-lg w-96" />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          className="border-2 rounded-lg w-96"
-        />
-      </form>
-    </div>
+      <div className="flex flex-col min-h-screen items-center font-main">
+        <h1 className="text-3xl">Login</h1>
+        <form
+          className="flex flex-col mt-2"
+          onSubmit={(e) => {
+            e.preventDefault();
+            loggin(e.target.email.value, e.target.password.value);
+          }}
+        >
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="border-2 rounded-lg w-96"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="border-2 rounded-lg w-96"
+          />
+          <button type="submit">OK</button>
+        </form>
+      </div>
+    </>
   );
 }

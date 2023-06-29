886d5b61d7f1182a4a650feafa7275f7013ee036
jelenal1 <jabbruh10@gmail.com>
2023-06-29 21:39:00 +0200

    style: made the focus outlines consistent to the rest, added button to sign up

--- react_project/src/page/Login.jsx
+++ react_project/src/page/Login.jsx
@@ -1,8 +1,11 @@
 import Navbar from "../components/Navbar";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+  const navigate = useNavigate();
   const loggin = async (email, password) => {
   await signInWithEmailAndPassword(auth, email, password);
   };
@@ -11,7 +14,7 @@ export default function Login() {
     <>
       <Navbar />
       <div className="flex flex-col min-h-screen items-center font-main">
-        <h1 className="text-3xl">Login</h1>
+        <h1 className="text-3xl mt-2">Login</h1>
         <form
           className="flex flex-col mt-2"
           onSubmit={(e) => {
@@ -23,16 +26,18 @@ export default function Login() {
           <input
             type="email"
             name="email"
-            className="border-2 rounded-lg w-96"
+            className="border-2 rounded-lg w-96 focus:outline focus:outline-2 focus:outline-[#F18CED]"
           />
           <label htmlFor="password">Password</label>
           <input
             type="password"
             name="password"
-            className="border-2 rounded-lg w-96"
+            className="border-2 rounded-lg w-96 focus:outline focus:outline-2 focus:outline-[#F18CED]"
           />
           <button type="submit">OK</button>
         </form>
+        <h2 className="text-xl mt-4">Don't have an account</h2>
+        <button onClick={() => navigate("/signup")} className="bg-[#F18CED] text-white mx-auto p-1 rounded-lg">Sign Up</button>
       </div>
     </>
   );
